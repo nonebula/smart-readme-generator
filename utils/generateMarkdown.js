4587f50c6f56e40ff8f23f011eff82165e3124b8
Joe Lloyd Higgs <joe@Joes-MacBook-Air.local>
2024-01-12 20:14:59 +0100

    Structure for README up and running, README populated.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,7 +2,45 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
-`;
+  ## Description
+  
+  ${data.description}
+  
+  ## Table of Contents
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+  
+  ${data.installation}
+  
+  ## Usage
+  
+  ${data.usage}
+  
+  ## License
+  
+  This project is licensed under the ${data.license} license.
+  
+  ## Contributing
+  
+  ${data.contributing}
+  
+  ## Tests
+  
+  ${data.tests}
+  
+  ## Questions
+  
+  GitHub: [${data.username}](https://github.com/${data.username})
+  
+  For additional questions, contact me via email: ${data.email}
+  `;
 }
 
 module.exports = generateMarkdown;
